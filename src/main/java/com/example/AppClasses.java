136d847077996c2adc2b04b2fd2d0c53b2a34a53
Junior Malacarne <juniormalacarne@hotmail.com>
2023-08-01 00:11:56 +0000

    Métodos Construtores e Strings (aulas 8 e 9)

--- src/main/java/com/example/AppClasses.java
+++ src/main/java/com/example/AppClasses.java
@@ -1,13 +1,27 @@
 package com.example;
 
 public class AppClasses {
-    public static void main(String[] args) {
-        Cliente cliente = new Cliente();
-        cliente.setRenda(3000);
-        cliente.setSexo('M');
-        cliente.setAnoNascimento(1987);;
-        System.out.println("Renda: " + cliente.getRenda());
-        System.out.println("Sexo: " + cliente.getSexo());
-        System.out.println("Ano de Nascimento: " + cliente.getAnoNascimento());
+    public static void main(String[] args) { // "void" seria o "tipo" do retorno do método ("main" é o método principal). Void significa que esse retorno é vazio, ou que ele não tem um "tipo" para retornar
+        Cliente cliente1 = new Cliente(); //método construtor para instanciar/criar um objeto de uma classe
+        cliente1.setNome("João");
+        cliente1.setRenda(3000);
+        cliente1.setSexo('M');
+        cliente1.setAnoNascimento(1987);;
+        System.out.println("Cliente 1");
+        System.out.println("Nome: " + cliente1.getNome());
+        System.out.println("Renda: " + cliente1.getRenda());
+        System.out.println("Sexo: " + cliente1.getSexo());
+        System.out.println("Ano de Nascimento: " + cliente1.getAnoNascimento());
+        System.out.println();
+
+        Cliente cliente2 = new Cliente(1500, 'F');
+        cliente2.setNome("Maria");
+        cliente2.setAnoNascimento(1991);
+        System.out.println("Cliente 2");
+        System.out.println("Nome: " + cliente2.getNome());
+        System.out.println("Renda: " + cliente2.getRenda());
+        System.out.println("Sexo: " + cliente2.getSexo());
+        System.out.println("Ano de Nascimento: " + cliente2.getAnoNascimento());
+        System.out.println();
     }
 }
--- src/main/java/com/example/Cliente.java
+++ src/main/java/com/example/Cliente.java
@@ -1,19 +1,41 @@
 package com.example;
 
 public class Cliente{
-    //variáveis criada no nível da classe, chamada de atributo. Qualquer função da classe consegue acessar essa variável
+    //variáveis criadas no nível da classe, chamadas de atributos. Qualquer função da classe consegue acessar essas variáveis
+    private String nome;
     private double renda; 
     private char sexo;
     private int anoNascimento;
+    private boolean especial;
+    
+    public Cliente(){ //método construtor. Não pode colocar o tipo do retorno (void, etc). Se o método não for incluso, fica implícito sem nada adicional
+        //construtuores podem ser usados para inicialização de variáveis com valores padrão de acordo com alguma lógica ou para imprimir mensagens, por exemplo
+        System.out.println("Criando um objeto Cliente.");
+    }
+
+    public Cliente(double renda, char sexo){
+        this(); //chamando um outro método construtor (para evitar duplicação de código e poder generalizar alterações)
+        //this.renda = renda; // jeito padrão para atribuir valor para o parâmetro
+        setRenda(renda); // chamando o método Set para aplicar a lógica do método ao atribuir valor ao parâmetro
+        setSexo(sexo);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
     public double getRenda(){
         return renda;
     }
 
     public void setRenda(double renda){
-        if(renda >= 0)    
+        if (renda >= 0)    
             this.renda = renda; //usar o this. ou diferenciar o nome do parâmetro
-        else System.out.println("A renda deve ser maior ou igual a zero");
+        else System.out.println("A renda deve ser maior ou igual a zero.");
     }
 
     public char getSexo() {
@@ -21,7 +43,9 @@ public class Cliente{
     }
 
     public void setSexo(char sexo) {
-        this.sexo = sexo;
+        if (sexo == 'M' || sexo == 'm' || sexo == 'F' || sexo == 'f')
+            this.sexo = sexo;
+        else System.out.println("O sexo deve ser 'M' ou 'F'.");
     }
 
     public int getAnoNascimento() {
@@ -32,7 +56,13 @@ public class Cliente{
         this.anoNascimento = anoNascimento;
     }
 
+    public boolean isEspecial() {
+        return especial;
+    }
 
+    public void setEspecial(boolean especial) {
+        this.especial = especial;
+    }
 
 
 }
