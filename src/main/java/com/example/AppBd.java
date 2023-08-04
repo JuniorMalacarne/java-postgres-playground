2e84253af8d0db1edf92ef3136fec7e083316736
Junior Malacarne <juniormalacarne@hotmail.com>
2023-08-04 20:53:12 +0000

    Método3 Aula4 - edescobrindo dados de uma tabela

--- src/main/java/com/example/AppBd.java
+++ src/main/java/com/example/AppBd.java
@@ -20,19 +20,53 @@ public class AppBd {
         try(var connection = getConnection()){
             // o objeto Connection é autocloseable (fecha sozinho ao finalizar o try com parênteses, sem precisar fazer isso manualmente)
             // pois é preciso fechar a conexão do banco de dados quando finalizar a execução do que está dentro do try
+            
             System.out.println("Conexão com Banco de Dados realizada com sucesso.");
+
             listarEstados(connection);
             localizarEstados(connection, "ES");
+            listaDadosTabela(connection, "produto");
         }catch (SQLException e) { // exceção, caso não consiga conectar ao Banco de Dados ou haja alguma falha na consulta sql
                 System.err.println("Não foi possível conectar ao banco de dados: " + e.getMessage());
         } 
     }
 
+    private void listaDadosTabela(Connection connection, String tabela) {
+        try {
+            var sql = "select * from " + tabela;
+            var statement = connection.prepareStatement(sql);
+            var result = statement.executeQuery();
+            
+            var metadada = result.getMetaData();
+            int cols = metadada.getColumnCount(); // obtendo metadados da tabela e a quantidade de colunas
+
+            for (int i = 1; i <= cols; i++) { // em SQL os parâmetros começam a contar do 1 (as colunas da tabela)
+                System.out.printf("%-25s | ", metadada.getColumnName(i)); // obtendo em formato String o conteúdo do nome de cada coluna da tabela
+            }
+            System.out.println();
+
+            while(result.next()){// o método next verifica se há alguma linha após
+                for (int i = 1; i <= cols; i++) { // em SQL os parâmetros começam a contar do 1 (as colunas da tabela)
+                    System.out.printf("%-25s | ", result.getString(i)); // obtendo em formato String o conteúdo de cada coluna da tabela
+                }
+                System.out.println();
+            }
+            System.out.println();
+        } catch (SQLException e) {
+            System.err.println("Não foi possível executar a consulta ao banco de dados: " + e.getMessage());
+        }
+    }
+
     private void localizarEstados(Connection connection, String uf) {
         try {
-            //var statement = connection.createStatement(); // createStatement não recebe uma SQL
-            //var result = statement.executeQuery("select * from estado where uf = '" + uf + "'"); // passar e concatenar strings como comandos SQL pode resultar na falha grave de segurança chamada SQL Injection
-            
+            /*
+            var statement = connection.createStatement(); // createStatement não recebe uma SQL
+            var result = statement.executeQuery("select * from estado where uf = '" + uf + "'"); 
+            *Passar e concatenar strings como comandos SQL pode resultar na falha grave de segurança chamada SQL Injection, 
+            isso quando o usuário pode inserir dados de consulta na aplicação. 
+            Uma opção para reforçar a segurança é sanitizar/validar os dados do usuário
+            */
+
             var sql = "select * from estado where uf = ?";
             var statement = connection.prepareStatement(sql); // o prepareStatement cria uma consulta preparada (ele verifica se a consulta está correta) e ela não vai ser alterada. Apenas pode-se alterar o valor dos parâmetros            
             statement.setString(1, uf); //os métodos set colocam a string no parâmetro (da sql) da ordem do número declarado
